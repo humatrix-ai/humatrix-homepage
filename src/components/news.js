b342ced129035e9bd332e5799252886d00d21a05
humatrix-ai <kentohonda@humatrix.jp>
2024-09-24 21:20:08 +0900

    fifth commit

--- src/components/news.js
+++ src/components/news.js
@@ -2,6 +2,7 @@
 import React from "react"
 import "../styles/all.css"
 import "../styles/news.css"
+import Logo from "../../static/century-systems-logo.gif"
 
 export default function News() {
     return (
@@ -11,24 +12,10 @@ export default function News() {
                     <h2>お知らせ</h2>
                     <div class="news-grid">
                         <div class="news-item">
-                            <img src="/api/placeholder/400/300" alt="準備中"></img>
                             <div class="news-item-content">
-                                <h3>準備中</h3>
-                                <p>準備中</p>
-                            </div>
-                        </div>
-                        <div class="news-item">
-                            <img src="/api/placeholder/400/300" alt="準備中"></img>
-                            <div class="news-item-content">
-                                <h3>準備中</h3>
-                                <p>準備中</p>
-                            </div>
-                        </div>
-                        <div class="news-item">
-                            <img src="/api/placeholder/400/300" alt="準備中"></img>
-                            <div class="news-item-content">
-                                <h3>準備中</h3>
-                                <p>準備中</p>
+                                <h3>センチュリー・システムズ株式会社様とのPoC実施開始</h3>
+                                <p>2024年7月より通信機器メーカーのセンチュリー・システムズ株式会社様とのPoCを開始致しました。<br/>
+                                本PoCは製品に関する技術的な問い合わせへの対応を目的としたAIチャットボットの有効性を確認することを目的としております。</p>
                             </div>
                         </div>
                     </div>
