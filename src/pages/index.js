f3e618ca737ec80735a5dbbfb465b4e9d46b3af4
humatrix-ai <kentohonda@humatrix.jp>
2024-09-24 20:29:42 +0900

    fourth commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,12 @@ import Services from "../components/services"
 import Achievements from "../components/achievements"
 import News from "../components/news"
 import Contact from "../components/contact"
+import SEO from "../components/seo"
 
 export default function Home({ data }) {
   return (
     <Layout>
+      <SEO title="ヒューマトリックス株式会社 | Humatrix" description="ヒューマトリックス株式会社はAIソリューション事業を展開し、「新たな可能性の母体」となることを目指しております。" />
       <Hero />
       <Services />
       <Achievements />
