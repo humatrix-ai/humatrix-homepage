7544b8d17fa38f4998cefa13486c8c88d1f148b0
humatrix-ai <kentohonda@humatrix.jp>
2025-04-01 15:11:26 +0900

    20250401

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,15 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: ["G-VVVM040JNP"],  // 控えておいた、測定IDを記載します。
+        pluginConfig: {
+          head: true  // headタグに記載されるようにコンフィグを設定します。
+        }
+      }
+    }
+  ],
 }
